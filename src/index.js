1a47c1047b91fed77e3f79b62c07114755ab47b9
Ali Darwish <a.d4rwich@outlook.com>
2021-08-06 19:38:16 +0200

    solving an error that would appear if you answer a specific question

--- src/index.js
+++ src/index.js
@@ -7,26 +7,12 @@ import { Provider } from "react-redux";
 import reducer from "./reducers";
 import middleware from "./middleware";
 import "semantic-ui-css/semantic.min.css";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import { PersistGate } from "redux-persist/lib/integration/react";
 
-// the following code I got from stack overflow answer as I didn't know how to persist the redux state
-const persistConfig = {
-  key: ["authedUser", "questions", "users"],
-  storage: storage,
-  whitelist: ["authedUser", "questions", "users"],
-};
-const pReducer = persistReducer(persistConfig, reducer);
-
-const store = createStore(pReducer, middleware);
-const persistor = persistStore(store);
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <App />
-    </PersistGate>
+    <App />
   </Provider>,
   document.getElementById("root")
 );
