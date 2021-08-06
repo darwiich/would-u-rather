147efbcf8cd24a8de8bd8139c5832ab786a779ab
Ali Darwish <a.d4rwich@outlook.com>
2021-08-06 19:47:35 +0200

    solving persist state issue

--- src/index.js
+++ src/index.js
@@ -7,12 +7,26 @@ import { Provider } from "react-redux";
 import reducer from "./reducers";
 import middleware from "./middleware";
 import "semantic-ui-css/semantic.min.css";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/lib/integration/react";
 
-const store = createStore(reducer, middleware);
+// the following code I got from stack overflow answer as I didn't know how to persist the redux state
+const persistConfig = {
+  key: ["authedUser", "questions", "users"],
+  storage: storage,
+  whitelist: ["authedUser", "questions", "users"],
+};
+const pReducer = persistReducer(persistConfig, reducer);
+
+const store = createStore(pReducer, middleware);
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
